93eca93571a0887e8af7f5f8c94d31c4fd1aad17
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-19 05:38:38 +0100

    Add files via upload

---
+++ Nextdns/NextDNS Routing.cmd
@@ -0,0 +1,2 @@
+netsh advfirewall firewall add rule name="Nextdns Block anexia-fra" program="C:\Program Files (x86)\NextDNS\NextDNSService.exe" dir=out description="Block anexia-fra ipv6" action=block enable=yes protocol=tcp remoteport=443 remoteip=2a00:11c0:e:ffff:1::d
+netsh advfirewall firewall add rule name="Nextdns Block zepto-fra" program="C:\Program Files (x86)\NextDNS\NextDNSService.exe" dir=out description="Block zepto-fra (ultralow2)" action=block enable=yes protocol=tcp remoteport=443 remoteip=194.45.101.249,2a0b:4341:704:24:5054:ff:fe91:8a6c
